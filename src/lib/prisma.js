c37e6eef19f85281d6e7133c3286fc5ec1eb3828
BeingAlmighty <manaskapoor66@gmail.com>
2026-07-02 18:41:18 +0530

    Updating Prisma configuration

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,13 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global;
 
 if (!globalForPrisma.prisma_v5) {
-  globalForPrisma.prisma_v5 = new PrismaClient();
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  globalForPrisma.prisma_v5 = new PrismaClient({ adapter });
 }
 
 export const prisma = globalForPrisma.prisma_v5;
